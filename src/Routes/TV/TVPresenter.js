5f230ad5add1b63ab20b7294b377b9ecfbcf4daa
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-04-05 12:59:11 +0900

    Presenter Commit

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,15 @@
-export default () => 'TV';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TVPresenter = ({ topRated, popular, airingTday, loading, error }) => null;
+
+TVPresenter.PropTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingTday: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
+};
+
+export default TVPresenter;
