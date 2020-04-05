228a81adb1d632b6b67d200ea397774c8bab391f
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-04-05 13:35:35 +0900

    Section Components Commit

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,15 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  null;
+import Section from 'Components/Section';
 
-HomePresenter.PropTypes = {
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title='Now Playing'>
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {/* 트루 && 트루 && 실행 가능 하면 실행 
+          a && b && console.log(a,b)
+      */}
+      {/* children 같은 경우ㅠ는 <Section>movie</Section> 여기서 movie를 뜻한다. 그럼 이값이 그리드에 넣어짐  */}
+      {upcoming && upcoming.length > 0 && (
+        <Section title='Upcoming Movies'>
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='Popular Movies'>
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
-  upcoming: PropTypes.array,
   popular: PropTypes.array,
-  error: PropTypes.string,
-  loading: PropTypes.bool.isRequired
+  upcoming: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
 };
 
 export default HomePresenter;
