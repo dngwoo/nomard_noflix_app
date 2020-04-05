e8b0ae62dfa148520ee55cd76b4f726801694abb
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-04-05 22:21:43 +0900

    Message Component Commit

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
 import Section from 'Components/Section';
 import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -39,6 +39,7 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
           ))}
         </Section>
       )}
+      {error && <Message color='#e743c' text={error} />}
     </Container>
   );
 
